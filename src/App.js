c98ce64ab3d044de26bf3ea2ffb89e86b47aabeb
workingjubilee <workingjubilee@gmail.com>
2019-03-11 01:25:37 -0700

    Complete basic introduction with useReducer and useEffect.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ContextOne } from './ContextOne';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+export function App() {
+  let { state, dispatch } = React.useContext(ContextOne);
+  React.useEffect(
+    () => {
+      document.body.style.backgroundColor = state.currentColor;
+    },
+    [state.currentColor]
+  );
+
+  let inc = () => dispatch({ type: "increment" });
+  let dec = () => dispatch({ type: "decrement" });
+  let reset = () => dispatch({ type: "reset" });
+  let setColor = color => () => dispatch({ type: "set-color", payload: color });
+
+
+  return (
+    <React.Fragment>
+      <div style={{ textAlign: "center" }}>
+        <p>
+          Current color is: <b>{state.currentColor}</b>
+        </p>
+        <p>
+          Current count: <b>{state.count}</b>
+        </p>
       </div>
-    );
-  }
-}
-
-export default App;
+      <div style={{ paddingTop: 40 }}>
+        <p>Count controls:</p>
+        <button onClick={inc}>Increment!</button>
+        <button onClick={dec}>Decrement!</button>
+      </div>
+      <div>
+        <p>Color controls:</p>
+        <button onClick={setColor("green")}>Change to green!</button>
+        <button onClick={setColor("papayawhip")}>Change to papayawhip!</button>
+      </div>
+      <div>
+        <p>Reset changes:</p>
+        <button onClick={reset}>Reset!</button>
+      </div>
+    </React.Fragment>
+  );
+};
